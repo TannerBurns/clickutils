1b0a0a842a71026e5f7b7eff3549ced2569111a3
Tanner Burns <tjburns102@gmail.com>
2020-02-16 10:48:59 -0600

    added more modular utils, v0.0.2

--- click_dynamics/utils.py
+++ click_dynamics/utils.py
@@ -2,9 +2,33 @@ import os
 import re
 import click
 
+from typing import Tuple
+
 
 CLICK_COMMAND_PATTERN = '\@click\.command\(.*\)[\s\S]*?(?=\n.*?=|def (.*)\(\):)'
 CLICK_GROUP_PATTERN = '\@click\.group\(.*\)[\s\S]*?(?=\n.*?=|def (.*)\(\):)'
+
+
+def get_directories_from_path(filepath: str) -> list:
+    '''
+    Return all directories found within the given directory
+    '''
+    return [ os.path.join(filepath, fp) for fp in os.listdir(filepath) 
+        if os.path.isdir(os.path.join(filepath, fp)) and '__pycache__' not in fp
+    ]
+
+
+def get_click_imports_from_path(filepath: str) -> Tuple[str, list]:
+    '''
+    Return the base path and all found groups or commands imports
+    '''
+    base_path = os.path.join(filepath, 'groups')
+    init_path = os.path.join(base_path, '__init__.py')
+    if os.path.exists(init_path):
+        with open(init_path, 'r') as fin:
+            data = fin.read()
+            return base_path, re.findall(CLICK_GROUP_PATTERN, data) + re.findall(CLICK_COMMAND_PATTERN, data)
+    return base_path, []
 
 
 def import_command_or_group_from_dotpath(base_group: click.core.Group, dotpath: str, import_name: str, verbose: bool= False) -> None:
@@ -15,16 +39,20 @@ def import_command_or_group_from_dotpath(base_group: click.core.Group, dotpath: 
         import_name - str - attempted click group or command name to import and set as new bg_command
         verbose - bool [default:False] - print verbose output
     '''
+    msg = ''
     try:
         exec_cmd = f'from {dotpath} import {import_name}; global base_group_command; base_group_command={import_name}'
-        exec(exec_cmd, globals())
-        bg_command = globals().get('base_group_command')
+        exec(exec_cmd)
+        bg_command = globals().get('base_group_command', None)
+        assert bg_command
         base_group.add_command(bg_command)
-        if verbose:
-            print(f'Successfully loaded and added command: {import_name}')
+        msg = f'Successfully loaded and added command: {import_name}'
+    except AssertionError:
+        msg = f'Failed to load: {dotpath}, {import_name}, failed to find any commands in globals'
     except Exception as err:    
-        if verbose:
-            print(f'Failed to load: {dotpath}, {import_name}, {err}')
+        msg = f'Failed to load: {dotpath}, {import_name}, {err}'
+    if verbose and msg:
+        print(msg)
 
 
 def load_commands_from_path(base_group: click.core.Group, filepath:str, verbose: bool= False) -> None:
@@ -34,16 +62,11 @@ def load_commands_from_path(base_group: click.core.Group, filepath:str, verbose:
         filepath - str - attempted path to find commands
         verbose - bool [default:False] - print verbose output
     '''
-    plugin_group_path = os.path.join(filepath, 'groups')
-    plugin_group_init_path = os.path.join(plugin_group_path, '__init__.py')
-    if os.path.exists(plugin_group_init_path):
-        with open(plugin_group_init_path, 'r') as fin:
-            raw = fin.read()
-            click_imports = re.findall(CLICK_GROUP_PATTERN, raw) + re.findall(CLICK_COMMAND_PATTERN, raw)
-            for import_name in click_imports:
-                dotpath = plugin_group_path.replace("/", ".")
-                import_command_or_group_from_dotpath(base_group, dotpath, import_name, verbose=verbose)
-                    
+    base_click_import_path, click_imports = get_click_imports_from_path(filepath)
+    for import_name in click_imports:
+        dotpath = base_click_import_path.replace("/", ".")
+        import_command_or_group_from_dotpath(base_group, dotpath, import_name, verbose=verbose)
+
 
 def load_commands_from_directory(base_group: click.core.Group, directory: str, verbose: bool= False) -> None:
     '''
@@ -52,10 +75,6 @@ def load_commands_from_directory(base_group: click.core.Group, directory: str, v
         directory - str - attempted path to find commands
         verbose - bool [default:False] - print verbose output
     '''
-    plugins = [
-        os.path.join(directory, fp) 
-        for fp in os.listdir(directory) 
-        if os.path.isdir(os.path.join(directory, fp)) and '__pycache__' not in fp
-    ]
-    for plugin_path in plugins:
-        load_commands_from_path(base_group, plugin_path, verbose=verbose)
+    click_group_and_command_paths = get_directories_from_path(directory)
+    for group_command_path in click_group_and_command_paths:
+        load_commands_from_path(base_group, group_command_path, verbose=verbose)
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 setup(
     name='click_dynamics',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(exclude=['examples']),
     include_package_data=True,
     description='Utilities for dynamically adding commands to click cli',
